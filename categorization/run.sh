a193f07bd7d6519bf0ce3a2c653ee428521040d0
Mateo Contenla <25857822+TaconeoMental@users.noreply.github.com>
2025-05-09 14:51:46 -0400

    No exponer puerto 80 de contenedor nginx

--- categorization/run.sh
+++ categorization/run.sh
@@ -2,6 +2,5 @@ docker run \
     --name nginx-categorization \
     --network evil \
     --rm \
-    -p 80:80 \
     -v ./site/:/usr/share/nginx/html nginx
 echo "[+] Categorization server started. Remember to enable configs in haproxy.cfg"
